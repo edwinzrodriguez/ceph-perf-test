00e53439b9f80cc5503801d99adc3841da22d377
Ed rodriguez <erodriguez@lazyk-ranch.com>
2026-07-31 09:48:09 -0400

    update `build-ceph-tentacle.sh`: add argument validation, dynamically handle multiple branches, unify installation prefixes, and clean up hardcoded paths

--- build-ceph-tentacle.sh
+++ build-ceph-tentacle.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 set -e
 
-for i in ceph-bench-tentacle-base ceph-bench-tentacle-osdc ceph-bench-tentacle-client-lock; do
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 <branch> [branch ...]" >&2
+    echo "Example: $0 ceph-bench-tentacle-base ceph-bench-tentacle-osdc" >&2
+    exit 1
+fi
+
+for i in "$@"; do
     if [ ! -e ~/git/$i ]; then
         pushd ~/git/ceph-tentacle
-        git worktree add ~/git/$i
+        git worktree add ~/git/$i -b $i origin/$i
         popd
     fi
     pushd ~/git/$i
-        git switch $i
+        git checkout origin/$i
         git submodule update --init --recursive
         if [ ! -e ~/git/$i/build ]; then
             ./do_cmake.sh -DUSE_TRACEFLOW=OFF \
             -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-            -DCMAKE_INSTALL_PREFIX=/usr/local/$i
+            -DCMAKE_INSTALL_PREFIX=/usr/local
         else
           pushd ~/git/$i/build
               cmake .. -DUSE_TRACEFLOW=OFF \
                 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-                -DCMAKE_INSTALL_PREFIX=/usr/local/$i
+                -DCMAKE_INSTALL_PREFIX=/usr/local
           popd
         fi
         pushd ~/git/$i/build
             ninja install
         popd
     popd
-    
+
     pushd ~/git/nfs-ganesha
     git pull
     popd
@@ -34,13 +40,13 @@ for i in ceph-bench-tentacle-base ceph-bench-tentacle-osdc ceph-bench-tentacle-c
     cmake ../nfs-ganesha/src/ -GNinja \
       -DUSE_TRACEFLOW=OFF \
       -DUSE_FSAL_CEPH=true \
-      -DCEPHFS_INCLUDE_DIR=/usr/local/$i/include \
-      -DCEPHFS_LIBRARY_DIR=/usr/local/$i/lib64 \
+      -DCEPHFS_INCLUDE_DIR=/usr/local/include \
+      -DCEPHFS_LIBRARY_DIR=/usr/local/lib64 \
       -DCMAKE_BUILD_TYPE=RelWithDebInfo \
-      -DCMAKE_INSTALL_PREFIX=/usr/local/$i
+      -DCMAKE_INSTALL_PREFIX=/usr/local
     ninja install
     popd
-    mkdir -pv /usr/local/$i/var/lib/nfs/ganesha
+    mkdir -pv /usr/local/var/lib/nfs/ganesha
 
     if [ ! -e ~/git/fio-$i ]; then
       pushd ~/git
@@ -50,9 +56,8 @@ for i in ceph-bench-tentacle-base ceph-bench-tentacle-osdc ceph-bench-tentacle-c
     pushd ~/git/fio-$i
       git checkout fio-3.36
       git submodule update --init --recursive
-      ./configure --prefix=/usr/local/$i --extra-cflags="-I/usr/local/$i/include -L/usr/local/$i/lib64"
+      ./configure --prefix=/usr/local --extra-cflags="-I/usr/local/include -L/usr/local/lib64"
       make install
     popd
 
 done
-
